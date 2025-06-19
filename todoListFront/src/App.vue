e4975628afbda5286c94ed90b51f9ca89b6ecdc0
NachoMartinezLozano <joseignacio.martinezl@um.es>
2025-06-19 19:35:00 +0200

    first commit

---
+++ todoListFront/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+
+<Header>
+
+</Header>
+
+
+</template>
+
+<style scoped>
+
+@import "tailwindcss";
+@plugin "daisyui";
+</style>
---
+++ todoListFront/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const nombreEmpresa = ref('TodoApp');
+
+</script>
+
+<template>
+    <header class="flex flex-col items-center justify-center bg-gray-900 w-full top-0 left-0 fixed p-4">
+        <h1 class="text-3xl text-white font-bold">{{ nombreEmpresa }}</h1>
+        <p class="text-lg text-gray-400 font-bold">Ten acceso a todas tus tareas del día desde nuestra aplicación</p>
+    </header>
+</template>
+
+<style>
+@import "tailwindcss";
+</style>
