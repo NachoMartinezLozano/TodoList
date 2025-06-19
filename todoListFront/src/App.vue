9b2953f5d28bfb508dd301870bdda8db29165c93
NachoMartinezLozano <joseignacio.martinezl@um.es>
2025-06-19 20:08:40 +0200

    Header vue

--- todoListFront/src/App.vue
+++ todoListFront/src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { ref } from 'vue'
 import Header from './components/Header.vue'
+const nombreEmpresa = ref('DataStorm')
 </script>
 
 <template>
 
-<Header>
+<Header></Header>
 
-</Header>
-
+<div class="flex flex-row w-full">
+  <section id="presentacion" class="flex flex-col">
+    <h2>Bienvenido a DataStorm!</h2>
+    <p>Desde CyberPulse Labs estamos muy concienciados con el correcto uso del tiempo.</p>
+    <p>Es por ello que hemos creado {{ nombreEmpresa }}, mediante la cual se podrá llevar a cabo un correcto control del desarrollo de las tareas.</p>
+    <!--En algún momento aquí pondré los botones para activar los distintos filtros de los todos-->
+  </section>
+</div>
 
 </template>
 
--- todoListFront/src/components/Header.vue
+++ todoListFront/src/components/Header.vue
@@ -2,14 +2,15 @@
 
 import { ref } from 'vue';
 
-const nombreEmpresa = ref('TodoApp');
+const nombreEmpresa = ref('Operación DataStorm');
 
 </script>
 
 <template>
     <header class="flex flex-col items-center justify-center bg-gray-900 w-full top-0 left-0 fixed p-4">
         <h1 class="text-3xl text-white font-bold">{{ nombreEmpresa }}</h1>
-        <p class="text-lg text-gray-400 font-bold">Ten acceso a todas tus tareas del día desde nuestra aplicación</p>
+        <p class="text-lg text-gray-400 font-bold">Gestiona todas tus tareas como agente de CyberPulse Labs</p>
+
     </header>
 </template>
 
