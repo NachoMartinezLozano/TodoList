3d7493d4c55af0dd5d588e21adfd89f8c84d09b4
NachoMartinezLozano <joseignacio.martinezl@um.es>
2025-06-20 09:30:47 +0200

    Esqueleto página

---
+++ todoListFront/src/components/Contenedor.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="flex justify-center">
+    <div class="grid grid-cols-2 mx-auto max-w-screen">
+        <section id="presentacion" class="flex flex-col p-4 mx-auto">
+            <h2 class="text-2xl font-bold m-4">Bienvenido a DataStorm!</h2>
+            <p class="text-lg font-bold m-4">Gestiona todas tus tareas como agente de CyberPulse Labs y consigue tu objetivo.</p>
+        </section>
+        
+        <section class="bg-gray-100 p-4 mx-auto">
+            <h3 class="text-xl font-bold mb-4">Filtros de tareas</h3>
+            <ul class="list-disc pl-5">
+                <li>Filtrar por estado</li>
+                <li>Filtrar por prioridad</li>
+                <li>Filtrar por fecha de vencimiento</li>
+            </ul>
+        </section>
+    </div>
+    </div>
+
+</template>
+
+<style>
+@import "tailwindcss";
+</style>
--- todoListFront/src/components/Header.vue
+++ todoListFront/src/components/Header.vue
@@ -7,7 +7,7 @@ const nombreEmpresa = ref('Operación DataStorm');
 </script>
 
 <template>
-    <header class="flex flex-col items-center justify-center bg-gray-900 w-full top-0 left-0 fixed p-4">
+    <header class="flex flex-col items-center justify-center w-full top-0 left-0 fixed p-4 mb-4">
         <h1 class="text-3xl text-white font-bold">{{ nombreEmpresa }}</h1>
         <p class="text-lg text-gray-400 font-bold">Gestiona todas tus tareas como agente de CyberPulse Labs</p>
 
